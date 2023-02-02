ea4f2546fa58de3c65b8d58bb6c7dbf8f590f14d
Marco Antonio Arruda <marco.nc.arruda@gmail.com>
2023-02-02 00:40:48 -0300

    feat: add id and delete endpoint

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const nanoid = require('nanoid')
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
@@ -13,16 +14,39 @@ const notes = []
 
 const NOTES_URL = '/api/v1/notes/'
 
-app.get(NOTES_URL, (req, res) => {
+const sleep = (ms) => new Promise(res => setTimeout(res, ms))
+
+app.get(NOTES_URL, async (req, res) => {
+  await sleep(1000)
+
   res.status(200).json(notes)
 })
 
-app.post(NOTES_URL, (req, res) => {
-  const note = req.body
+app.post(NOTES_URL, async (req, res) => {
+  const note = {
+    id: nanoid(),
+    ...req.body,
+  }
 
   notes.push(note)
 
+  await sleep(1000)
+
   res.status(201).json(note)
+})
+
+app.delete(`${NOTES_URL}:id/`, async (req, res) => {
+  const noteId = req.params.id
+
+  const index = notes.findIndex((note => note.id === noteId))
+
+  if (index === -1) return res.status(404).send()
+
+  await sleep(1000)
+
+  notes.splice(index, 1)
+
+  res.status(204).send()
 })
 
 const port = 3000
